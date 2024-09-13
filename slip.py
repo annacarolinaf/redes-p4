292ef4b3cd6fcf70968b836ba95391bd45430bc0
annacarolinaf <annacarolinafarias@hotmail.com>
2024-09-13 11:07:12 -0300

    T4

--- slip.py
+++ slip.py
@@ -43,22 +43,30 @@ class Enlace:
     def __init__(self, linha_serial):
         self.linha_serial = linha_serial
         self.linha_serial.registrar_recebedor(self.__raw_recv)
+        self.buffer = b''
 
     def registrar_recebedor(self, callback):
         self.callback = callback
 
     def enviar(self, datagrama):
-        # TODO: Preencha aqui com o código para enviar o datagrama pela linha
-        # serial, fazendo corretamente a delimitação de quadros e o escape de
-        # sequências especiais, de acordo com o protocolo CamadaEnlace (RFC 1055).
-        pass
+        datagrama_codificado = datagrama.replace(b'\xDB', b'\xDB\xDD')\
+                                        .replace(b'\xC0', b'\xDB\xDC')
+        datagrama_completo = b'\xC0' + datagrama_codificado + b'\xC0'
+        self.linha_serial.enviar(datagrama_completo)
 
     def __raw_recv(self, dados):
-        # TODO: Preencha aqui com o código para receber dados da linha serial.
-        # Trate corretamente as sequências de escape. Quando ler um quadro
-        # completo, repasse o datagrama contido nesse quadro para a camada
-        # superior chamando self.callback. Cuidado pois o argumento dados pode
-        # vir quebrado de várias formas diferentes - por exemplo, podem vir
-        # apenas pedaços de um quadro, ou um pedaço de quadro seguido de um
-        # pedaço de outro, ou vários quadros de uma vez só.
-        pass
+        self.buffer += dados
+        quadros = self.buffer.split(b'\xC0')
+        self.buffer = quadros[-1]
+
+        for quadro in quadros[:-1]:
+            if quadro:
+                try:
+                    datagrama_decodificado = quadro.replace(b'\xDB\xDC', b'\xC0')\
+                                                   .replace(b'\xDB\xDD', b'\xDB')
+                    if self.callback:
+                        self.callback(datagrama_decodificado)
+                except Exception:
+                    import traceback
+                    traceback.print_exc()
+
